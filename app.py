a56cef2ea15365f706730fea73e11637cd494afc
Ashwin21 <24bec10074@cuchd.in>
2026-06-05 13:19:04 +0530

    try block fix

--- app.py
+++ app.py
@@ -94,17 +94,15 @@ if predict_btn:
         st.markdown("## 📊 Prediction Results")
         st.info(f"📟 Predicted Fault Code: {prediction[0]}")
 
-            # Load encoder
-            try:
-                fault_encoder = pickle.load(open("fault_encoder.pkl", "rb"))
+                  try:
+        fault_encoder = pickle.load(open("fault_encoder.pkl", "rb"))
 
-                fault_name = fault_encoder.inverse_transform(prediction)
+        fault_name = fault_encoder.inverse_transform(prediction)
 
-            st.success(f"🛠 Faulty Component: {fault_name[0]}")
+        st.success(f"🛠️ Faulty Component: {fault_name[0]}")
         st.balloons()
 
-            except Exception as e:
-                st.error(f"Encoder Error: {e}")
-            st.markdown("---")
+    except Exception as e:
+        st.error(f"Encoder Error: {e}")
 st.markdown("---")
 st.markdown("Made by Ashwin Dubey | DiagnostiX AI 🚀")
